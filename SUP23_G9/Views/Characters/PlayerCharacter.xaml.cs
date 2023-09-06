c60ecb855d3c90579afd831c001c3b9e4057a4a0
mavosy <mavo2202@student.miun.se>
2023-09-06 15:19:53 +0200

    -Rörelse för spelare och mob funkar -kollision funkar helt ok -Städat och fixat renare kod

--- SUP23_G9/Views/Characters/PlayerCharacter.xaml.cs
+++ SUP23_G9/Views/Characters/PlayerCharacter.xaml.cs
@@ -24,102 +24,13 @@ namespace SUP23_G9.Views.Characters
     /// </summary>
     public partial class PlayerCharacter : Character, INotifyPropertyChanged
     {
-        private readonly Timer? _timer;
-
-        Key _currentKey;
         public PlayerCharacter()
         {
-            //LeftCoordinates = 0;
-            //TopCoordinates = 0;
 
-            //_timer = new Timer();
-            //_timer.Interval = 100;
-            //_timer.Elapsed += (_, _) => MovePlayerCharacter();
-            //_timer.Start();
-
-
-            //ArrowKeyDownCommand = new RelayCommand(x => ExecuteArrowKeyDown());
         }
 
-        //metod som gör att något rör sig
-
-        //Skapa ett relaycommand hanterar eventhandlers keydown tex
-        //public ICommand ArrowKeyDownCommand { get; }
-        public double LeftCoordinates { get; set; } = 50;
+        //dessa properties binder till playerCharacter UC i GameView UI
+        public double LeftCoordinates { get; set; } = 50; 
         public double TopCoordinates { get; set; } = 50;
-
-
-        //private void Character_KeyDown(object sender, KeyEventArgs e)
-        //{
-        //    //_timer.Elapsed += (_, _) => MovePlayerCharacter(e.Key, characterSpeed: 20);
-
-        //    switch (e.Key)
-        //    {
-        //        case Key.Left:
-        //            //_currentKey = Key.Left;
-        //            MessageBox.Show("Vänster");
-        //            break;
-        //        case Key.Right:
-        //            _currentKey = Key.Right;
-        //            break;
-        //        case Key.Up:
-        //            _currentKey = Key.Up;
-        //            break;
-        //        case Key.Down:
-        //            _currentKey = Key.Down;
-        //            break;
-        //    }
-        //}
-        //private void ExecuteArrowKeyDown()
-        //{
-        //    if (obj is Key key)
-        //    {
-        //        if (key == currentKey)
-        //        {
-        //            // The same key is pressed again, so stop the timer to prevent rapid movements
-        //            _timer.Stop();
-        //            currentKey = Key.None; // Reset the current key
-        //        }
-        //        else
-        //        {
-        //            // Start the timer when the arrow key is pressed
-        //            currentKey = key;
-        //            _timer.Start();
-        //        }
-        //    }
-        //}
-
-        //private Key _currentKey; // To keep track of the last pressed key
-
-        //public void MovePlayerCharacter(Key key)
-        //{
-        //    //Key currentKey = key;
-        //    //if (key == currentKey)
-        //    //{
-        //    //    _timer.Stop();
-        //    //    currentKey = Key.None;
-        //    //}
-        //    //else
-        //    //{
-        //    //    _timer.Start();
-
-        //    //int characterSpeed = 20;
-        //    //switch (key)
-        //    //{
-        //    //    case Key.A:
-        //    //        MessageBox.Show("Funkar");
-        //    //        //LeftCoordinates -= characterSpeed;
-        //    //        break;
-        //    //    case Key.D:
-        //    //        LeftCoordinates += characterSpeed;
-        //    //        break;
-        //    //    case Key.W:
-        //    //        TopCoordinates -= characterSpeed;
-        //    //        break;
-        //    //    case Key.S:
-        //    //        TopCoordinates += characterSpeed;
-        //    //        break;
-        //    //}
-        //}
     }
 }
--- SUP23_G9/Views/GameView.xaml.cs
+++ SUP23_G9/Views/GameView.xaml.cs
@@ -20,6 +20,7 @@ using System.Windows.Shapes;
 using System.Windows.Threading;
 using static System.Net.Mime.MediaTypeNames;
 using Application = System.Windows.Application;
+using Image = System.Windows.Controls.Image;
 
 namespace SUP23_G9.Views
 {
@@ -28,21 +29,37 @@ namespace SUP23_G9.Views
     /// </summary>
     public partial class GameView : UserControl, INotifyPropertyChanged
     {
-
+        #region Field-variables
         Timer? _timer = new();     //nyar upp en ny timer från namespace System.Timers
-        bool _leftButtonIsDown, _rightButtonIsDown, _upButtonIsDown, _downButtonIsDown;   //variabel för om en given knapp är nertryckt eller släppt
+        
+        bool _leftButtonIsDown, _rightButtonIsDown, _upButtonIsDown, _downButtonIsDown;   //bool-variabel för om en given knapp är nertryckt eller släppt
+        
         readonly int _playerSpeed = 5;    //sätter spelarens hastighet
-        //readonly int mobSpeed = 5;
+        int _mobSpeed = 5;              //sätter mobens hastighet 
 
+        //fields för bild-URIs
+        readonly string _krakenLeft = "/Views/Components/Images/Happy_Kraken_Left.bmp";
+        readonly string _krakenRight = "/Views/Components/Images/Happy_Kraken_Right.bmp";
+        readonly string _pirateShip1Left = "/Views/Components/Images/PirateShip1_Left.bmp";
+        readonly string _pirateShip1Right = "/Views/Components/Images/PirateShip1_Right.bmp.bmp";
+        #endregion
+
+        #region Constructors
         public GameView()
         {
             InitializeComponent();
-            player.Focus();
-            //PlayerCharacter character = new PlayerCharacter();
-            _timer.Interval = 10;
-            _timer.Elapsed += MovePlayerEvent;
-            _timer.Start();
+            player.Focus();     //försöker fokusera på spelaren, behövs för KeyDown-event
+
+            _timer.Interval = 10;    //sätter ett intervall i millisekunder för hur ofta MovePlayerEvent ska köras
+            _timer.Elapsed += MovePlayerEvent;     //kör MovePlayerEvent varje gång interval ska börja om
+            _timer.Elapsed += MoveMobEvent;     //kör MoveMobEvent varje gång interval ska börja om
+
+            //_timer.Elapsed += MoveMobEvent;
+            _timer.Start();    //startar timer
         }
+        #endregion
+
+        #region KeyEvents
         /// <summary>
         /// Fångar KeyDown-eventet när man trycker ner tangenten
         /// </summary>
@@ -50,17 +67,20 @@ namespace SUP23_G9.Views
         /// <param name="e"></param>
         private void player_KeyDown(object sender, KeyEventArgs e)
         {
-            switch (e.Key)
+            switch (e.Key)   //I KeyEventArgs finns värdet "Key" som helt enkelt är vilken knapp som har tryckts ner, typ P, R, 8, Enter, NumLock etc. Switchar på det och sätter bool till true
             {
-                case Key.A:
+                case Key.A:        //här kan man ange typ vilka tangenter som helst, det ändrar kontrollen till spelet. Kanske ha userInput så man kan ändra själv?
                     _leftButtonIsDown = true;
                     break;
+
                 case Key.D:
                     _rightButtonIsDown = true;
                     break;
+
                 case Key.W:
                     _upButtonIsDown = true;
                     break;
+
                 case Key.S:
                     _downButtonIsDown = true;
                     break;
@@ -77,17 +97,20 @@ namespace SUP23_G9.Views
         /// <param name="e"></param>
         private void player_KeyUp(object sender, KeyEventArgs e)
         {
-            switch (e.Key)
+            switch (e.Key) //switchar på e.Key och sätter booleans för *ButtonIsDown till false när man släpper tangent
             {
                 case Key.A:
                     _leftButtonIsDown = false;
                     break;
+
                 case Key.D:
                     _rightButtonIsDown = false;
                     break;
+
                 case Key.W:
                     _upButtonIsDown = false;
                     break;
+
                 case Key.S:
                     _downButtonIsDown = false;
                     break;
@@ -96,113 +119,148 @@ namespace SUP23_G9.Views
                     break;
             }
         }
+        #endregion
 
+        #region Methods for border check
+        /// <summary>
+        /// Kollar om en 'image' är tillräckligt långt från kanten eller inte.
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="distanceToEdge"></param>
+        /// <returns></returns>
+        private bool IsNotAtLeftEdge(Image image, int distanceToEdge)
+        {
+            return Dispatcher.Invoke(() => Canvas.GetLeft(image) > distanceToEdge);
+        }
+
+        /// <summary>
+        /// Kollar om en 'image' är tillräckligt långt från kanten eller inte.
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="distanceToEdge"></param>
+        /// <returns></returns>
+        private bool IsNotAtRightEdge(Image image, int distanceToEdge)
+        {
+            return Dispatcher.Invoke(() => Canvas.GetLeft(image) + (image.Width) < (Application.Current.MainWindow.ActualWidth - 70) - distanceToEdge);
+        }
+
+        /// <summary>
+        /// Kollar om en 'image' är tillräckligt långt från kanten eller inte.
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="distanceToEdge"></param>
+        /// <returns></returns>
+        private bool IsNotAtTopEdge(Image image, int distanceToEdge)
+        {
+            return Dispatcher.Invoke(() => Canvas.GetTop(image) > distanceToEdge);
+        }
+
+        /// <summary>
+        /// Kollar om en 'image' är tillräckligt långt från kanten eller inte.
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="distanceToEdge"></param>
+        /// <returns></returns>
+        private bool IsNotAtBottomEdge(Image image, int distanceToEdge)
+        {
+            return Dispatcher.Invoke(() => Canvas.GetTop(image) + (image.Height) < (Application.Current.MainWindow.ActualHeight - 93) - distanceToEdge);
+        }
+        #endregion
+
+        #region Movement
         /// <summary>
         /// Förflyttar spelare beroende på vilken tangentbordsknapp man trycker på
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void MovePlayerEvent(object? sender, EventArgs e)
+        private void MovePlayerEvent(object? sender, ElapsedEventArgs e)
         {
-            if (_leftButtonIsDown && Dispatcher.Invoke(() => Canvas.GetLeft(player) > 5))
+            if (_leftButtonIsDown && IsNotAtLeftEdge(player, 5))
             {
-                Dispatcher.Invoke(() => Canvas.SetLeft(player, Canvas.GetLeft(player) - _playerSpeed)); //sätter ny x-koordinat till den förra koordinaten minus _playerSpeed
-                //todo fixa lagget
-                //koden gör att Kraken vänder på sig beroende på vilket håll den rör sig åt (vänster eller höger). Gör tyvärr att det laggar lite.
-                //Dispatcher.Invoke(() => player.Source = new BitmapImage(new Uri(@"/Views/Components/Images/Happy_Kraken_Left.bmp", UriKind.Relative))); 
+                SetLeftMovement(player, _playerSpeed); //sätter ny x-koordinat till den förra koordinaten MINUS _playerSpeed
+                ChangeImage(player, _krakenLeft);        //koden gör att Kraken vänder på sig beroende på vilket håll den rör sig åt (vänster eller höger)
             }
-            if (_rightButtonIsDown && Dispatcher.Invoke(() => Canvas.GetLeft(player) + (player.Width) < Application.Current.MainWindow.Width - 70))
-            {
-                Dispatcher.Invoke(() => Canvas.SetLeft(player, Canvas.GetLeft(player) + _playerSpeed));
-                //todo fixa lagget
-                //koden gör att Kraken vänder på sig beroende på vilket håll den rör sig åt (vänster eller höger). Gör tyvärr att det laggar lite.
-                //Dispatcher.Invoke(() => player.Source = new BitmapImage(new Uri(@"/Views/Components/Images/Happy_Kraken_Right.bmp", UriKind.Relative)));
 
-            }
-            if (_upButtonIsDown && Dispatcher.Invoke(() => Canvas.GetTop(player) > 5))
+            if (_rightButtonIsDown && IsNotAtRightEdge(player, 5))
             {
-                Dispatcher.Invoke(() => Canvas.SetTop(player, Canvas.GetTop(player) - _playerSpeed));
+                SetLeftMovement(player, -_playerSpeed);   //sätter ny x-koordinat till den förra koordinaten PLUS _playerSpeed
+                ChangeImage(player, _krakenRight);       //koden gör att Kraken vänder på sig beroende på vilket håll den rör sig åt (vänster eller höger)
             }
-            if (_downButtonIsDown && Dispatcher.Invoke(() => Canvas.GetTop(player) + (player.Height) < Application.Current.MainWindow.Height - 100))
-            {
-                Dispatcher.Invoke(() => Canvas.SetTop(player, Canvas.GetTop(player) + _playerSpeed));
-            }
-            //switch (_move)
-            //{
-            //    case Key.A:
-            //        Dispatcher.Invoke(() => Canvas.SetLeft(cube, Canvas.GetLeft(cube) - cubeSpeed));
-            //        break;
-            //    case Key.D:
-            //        Dispatcher.Invoke(() => Canvas.SetLeft(cube, Canvas.GetLeft(cube) + cubeSpeed));
-            //        break;
-            //    case Key.W:
-            //        Dispatcher.Invoke(() => Canvas.SetTop(cube, Canvas.GetTop(cube) - cubeSpeed));
-            //        break;
-            //    case Key.S:
-            //        Dispatcher.Invoke(() => Canvas.SetTop(cube, Canvas.GetTop(cube) + cubeSpeed));
-            //        break;
 
-            //    default:
-            //        break;
-            //}
+            if (_upButtonIsDown && IsNotAtTopEdge(player, 5))
+            {
+                SetTopMovement(player, _playerSpeed);       //sätter ny y-koordinat till den förra koordinaten MINUS _playerSpeed
+            }
+
+            if (_downButtonIsDown && IsNotAtBottomEdge(player, 5))
+            {
+                SetTopMovement(player, -_playerSpeed);      //sätter ny x-koordinat till den förra koordinaten PLUS _playerSpeed
+            }
+            CollisionCheckTest();
         }
 
+        /// <summary>
+        /// Sätter horizontell rörelse och hastighet
+        /// </summary>
+        /// <param name="imageComponent"></param>
+        /// <param name="speed"></param>
+        private void SetLeftMovement(Image imageComponent, int speed)
+        {
+            Dispatcher.Invoke(() => Canvas.SetLeft(imageComponent, Canvas.GetLeft(imageComponent) - speed));
+        }
 
+        /// <summary>
+        /// Sätter vertikal rörelse och hastighet
+        /// </summary>
+        /// <param name="imageComponent"></param>
+        /// <param name="speed"></param>
+        private void SetTopMovement(Image image, int speed)
+        {
+            Dispatcher.Invoke(() => Canvas.SetTop(image, Canvas.GetTop(image) - speed));
+        }
 
-        //private void MovePlayerCharacter(Key key)
-        //{
-        //    int cubeSpeed = 10;
-        //    if (key == Key.A)
-        //    {
-        //        Dispatcher.Invoke(() =>
-        //        {
-        //            Canvas.SetLeft(cube, Canvas.GetLeft(cube) - cubeSpeed);
-        //        });
-        //    }
-        //    if (true)
-        //    {
-        //        Dispatcher.Invoke(() =>
-        //        {
-        //            Canvas.SetLeft(cube, Canvas.GetLeft(cube) - cubeSpeed);
-        //        });
-        //    }            
-        //    if (true)
-        //    {
-        //        Dispatcher.Invoke(() =>
-        //        {
-        //            Canvas.SetLeft(cube, Canvas.GetLeft(cube) - cubeSpeed);
-        //        });
-        //    }
-        //    if (true)
-        //    {
-        //        Dispatcher.Invoke(() =>
-        //        {
-        //            Canvas.SetLeft(cube, Canvas.GetLeft(cube) - cubeSpeed);
-        //        });
-        //    }
-        //}
+        /// <summary>
+        /// Byter en bildkälla i UI till en ny
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="newImageURI"></param>
+        private void ChangeImage(Image image, string newImageURI)
+        {
+            Dispatcher.Invoke(() => image.Source = new BitmapImage(new Uri(@newImageURI, UriKind.Relative)));   //byter imagesource till ny källa
+        }
 
-        //private void MovePlayerCharacter(Key key)
-        //{
-        //    int cubeSpeed = 3;
-        //    switch (key)
-        //    {
-        //        case Key.A:
-        //            Dispatcher.Invoke(() => Canvas.SetLeft(cube, Canvas.GetLeft(cube) - cubeSpeed));
-        //            break;
-        //        case Key.D:
-        //            Dispatcher.Invoke(() => Canvas.SetLeft(cube, Canvas.GetLeft(cube) + cubeSpeed));
-        //            break;
-        //        case Key.W:
-        //            Dispatcher.Invoke(() => Canvas.SetTop(cube, Canvas.GetTop(cube) - cubeSpeed));
-        //            break;
-        //        case Key.S:
-        //            Dispatcher.Invoke(() => Canvas.SetTop(cube, Canvas.GetTop(cube) + cubeSpeed));
-        //            break;
+        private void MoveMobLeft(Image mobImage)
+        {
+            SetLeftMovement(mobImage, _mobSpeed);
 
-        //        default:
-        //            break;
-        //    }
-        //}
+            bool mobIsAtLeftEdge = !IsNotAtLeftEdge(pirateShip1, 5);
+            bool mobIsAtRightEdge = !IsNotAtRightEdge(pirateShip1, 5);
+
+            if (mobIsAtLeftEdge)
+            {
+                _mobSpeed = -_mobSpeed;   //byter håll 
+            }
+            if (mobIsAtRightEdge)
+            {
+                _mobSpeed = -_mobSpeed;   //byter håll
+            }
+        }
+
+        private void CollisionCheckTest()    //Funkar men inte helt felfritt
+        {
+            bool collisionX = Dispatcher.Invoke(() => Canvas.GetLeft(player) < Canvas.GetLeft(pirateShip1) + (pirateShip1.Width)) && Dispatcher.Invoke(() => Canvas.GetLeft(player) + (player.Width) > Canvas.GetLeft(pirateShip1));
+            bool collisionY = Dispatcher.Invoke(() => Canvas.GetTop(player) < Canvas.GetTop(pirateShip1) + (pirateShip1.Height)) && Dispatcher.Invoke(() => Canvas.GetTop(player) + (player.Height) > Canvas.GetTop(pirateShip1));
+
+            if (collisionX && collisionY)
+            {
+                MessageBox.Show("The pirate ship was dragged down to the dark depths of the ocean, crushed by slimy tendrils and eaten for dinner");
+            }
+        }
+
+        private void MoveMobEvent(object? sender, ElapsedEventArgs e)
+        {
+            MoveMobLeft(pirateShip1);
+        } 
+        #endregion
     }
 }
