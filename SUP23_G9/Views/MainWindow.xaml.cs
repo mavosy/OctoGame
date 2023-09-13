2d51c790253206670b122c09563accb2b7bf6364
jonnaastiernstrom <jonnaastiernstrom@hotmail.com>
2023-09-13 14:55:39 +0200

    Fixade ett litet fel

--- SUP23_G9/Views/MainWindow.xaml.cs
+++ SUP23_G9/Views/MainWindow.xaml.cs
@@ -8,11 +8,26 @@ namespace SUP23_G9.Views
 {
     public partial class MainWindow : Window
     {
+        private GameOverView gameOverView; // Skapa en privat medlemsvariabel för GameOverView
         public MainWindow()
         {
             InitializeComponent();
             DataContext = new MainViewModel();
             //DataContext = new TimerViewModel(30);  // 1 minuters nedräkning
+
+            // Skapa en instans av GameOverView
+            gameOverView = new GameOverView();
+
+            // Lägg till en händelsehanterare för MainWindow's Closing-händelse
+            Closing += MainWindow_Closing;
+        }
+
+        // Händelsehanterare för MainWindow's Closing-händelse
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            // Visa GameOverView och stäng MainWindow
+            gameOverView.Show();
+            
         }
     }
 }
