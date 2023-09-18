e133e0b0e1a9c84ec90846f5a135b7f255abd45c
jonnaastiernstrom <jonnaastiernstrom@hotmail.com>
2023-09-18 17:00:05 +0200

    3 game over window

--- SUP23_G9/Views/MainWindow.xaml.cs
+++ SUP23_G9/Views/MainWindow.xaml.cs
@@ -9,11 +9,13 @@ namespace SUP23_G9.Views
     public partial class MainWindow : Window
     {
         private GameOverView gameOverView; // Skapa en privat medlemsvariabel för GameOverView
+        private bool isGameOverShown = false; // Boolesk variabel för att kontrollera om GameOverView har visats
+
         public MainWindow()
         {
             InitializeComponent();
             DataContext = new MainViewModel();
-            //DataContext = new TimerViewModel(30);  // 1 minuters nedräkning
+            //DataContext = new TimerViewModel(30);  // 1 minuts nedräkning
 
             // Skapa en instans av GameOverView
             gameOverView = new GameOverView();
@@ -25,9 +27,13 @@ namespace SUP23_G9.Views
         // Händelsehanterare för MainWindow's Closing-händelse
         private void MainWindow_Closing(object sender, CancelEventArgs e)
         {
-            // Visa GameOverView och stäng MainWindow
-            gameOverView.Show();
-            
+            // Kontrollera om tiden har gått ut och om GameOverView redan har visats
+            if (!isGameOverShown)
+            {
+                // Visa GameOverView och markera att det har visats
+                gameOverView.Show();
+                isGameOverShown = true;
+            }
         }
     }
 }
