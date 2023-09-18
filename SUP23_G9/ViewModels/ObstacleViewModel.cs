e772b3266ec2790059f6418cb260dd2369266f3d
Paulina Sedzinski <paulina.sedzinski@studerande.karlskogafhs.se>
2023-09-18 13:16:22 +0200

    Obstacleviewmodel

---
+++ SUP23_G9/ViewModels/ObstacleViewModel.cs
@@ -0,0 +1,46 @@
+﻿using SUP23_G9.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace SUP23_G9.ViewModels
+{
+    public class ObstacleViewModel : BaseViewModel
+    {
+        public ObstacleViewModel()
+        {
+            Width = 50;
+            Height = 50;
+
+            LoadObstacleImageProcessing();
+        }
+        /// <summary>
+        /// X-koordinat för obstacle
+        /// </summary>
+        public double Left { get; set; }
+        /// <summary>
+        /// Y-koordinat för obstacle
+        /// </summary>
+        public double Top { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public BitmapImage ObstacleImage { get; set; }
+        private void LoadObstacleImageProcessing()
+        {
+            BitmapImage image = new BitmapImage();
+
+            image.BeginInit();
+            image.UriSource = new Uri("pack://application:,,,/SUP23_G9;component/Views/Components/Images/PirateShip1_Right.bmp");
+            image.DecodePixelWidth = 50;
+            image.CacheOption = BitmapCacheOption.OnLoad;
+            image.EndInit();
+
+            ObstacleImage = image;
+        }
+    }
+}
