57cde83dc204e9c131341face018205bf281a291
jonnaastiernstrom <jonnaastiernstrom@hotmail.com>
2023-09-22 11:37:29 +0200

    Animation: Ship Obstacle

--- SUP23_G9/ViewModels/Base/BaseViewModel.cs
+++ SUP23_G9/ViewModels/Base/BaseViewModel.cs
@@ -1,16 +1,25 @@
 ﻿using PropertyChanged;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
 
 namespace SUP23_G9.ViewModels.Base
 {
     [AddINotifyPropertyChangedInterface]
 
-    public abstract class BaseViewModel
+    public abstract class BaseViewModel : INotifyPropertyChanged
     {
         public string InstanceID { get; } = Guid.NewGuid().ToString();
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- SUP23_G9/ViewModels/ShipViewModel.cs
+++ SUP23_G9/ViewModels/ShipViewModel.cs
@@ -1,58 +1,54 @@
 ﻿using SUP23_G9.ViewModels.Base;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Media.Imaging;
-using System.Windows.Threading;
 
 namespace SUP23_G9.ViewModels
 {
     public class ShipViewModel : BaseViewModel
     {
-        //DispatcherTimer _shipTimer;
+        private bool _isAnimating = true; 
 
         public ShipViewModel()
         {
             Width = 50;
             Height = 50;
-            //FlipImageX = 1.0;
-
-            //_shipTimer = new DispatcherTimer();
-            //_shipTimer.Interval = TimeSpan.FromSeconds(2);
-            //_shipTimer.Tick += ShipDanceEvent;
-            //_shipTimer.Start();
-
             LoadShipImageProcessing();
+        }
+
+        public bool IsAnimating
+        {
+            get { return _isAnimating; }
+            set
+            {
+                _isAnimating = value;
+                OnPropertyChanged(nameof(IsAnimating));
+            }
         }
 
         /// <summary>
         /// X-koordinat för skeppen
         /// </summary>
         public double Left { get; set; }
+
         /// <summary>
         /// Y-koordinat för skeppen
         /// </summary>
         public double Top { get; set; }
+
         /// <summary>
         /// Skeppens bredd
         /// </summary>
         public int Width { get; set; }
+
         /// <summary>
         /// Skeppens höjd
         /// </summary>
         public int Height { get; set; }
+
         /// <summary>
         /// Bild för skeppen
         /// </summary>
         public BitmapImage ShipImage { get; set; }
-        /// <summary>
-        /// Sätter vilket håll skeppen är vänd mot i horisontellt led (1.0=original, -1.0=spegel)
-        /// </summary>
-        //public double FlipImageX { get; set; } 
 
         /// <summary>
         /// Processerar och cachar bild för skepp
@@ -69,25 +65,5 @@ namespace SUP23_G9.ViewModels
 
             ShipImage = image;
         }
-        /// <summary>
-        /// Får skeppen att vända på sig med jämna intervaller
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        //private void ShipDanceEvent(object? sender, EventArgs e)
-        //{
-        //    if (FlipImageX == 1.0)
-        //    {
-        //        FlipImageX = -1.0;
-        //    }
-        //    else if (FlipImageX == -1.0)
-        //    {
-        //        FlipImageX = 1.0;
-        //    }
-        //}
-        //public void StopShipTimer()
-        //{
-        //    _shipTimer.Stop();
-        //}
     }
 }
